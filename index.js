4dcfa646d0778e7d9b23950655be9f18e1a645c7
Mrkishansharma <kishansharma6377@gmail.com>
2023-04-02 17:46:14 +0530

    portfolio

---
+++ index.js
@@ -0,0 +1,65 @@
+let i=0;
+let txt="NodeJS Developer.";
+let speed=75;
+
+typeWriter()
+
+function typeWriter() {
+    if(i < txt.length){
+      document.getElementById("user-detail-name").style.color="aqua";
+      document.getElementById("user-detail-name").style.fontWeight="Bold";
+      document.getElementById("demo1").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+    else {
+      // reset the index when it reaches the end of the text
+      i = 0;
+      // clear the text
+      document.getElementById("demo1").innerHTML = "";
+      // make the recursive call
+      document.getElementById("user-detail-name").style.color="white";
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+
+  var myNav = document.getElementById('nav-menu');
+  var myNav2 = document.getElementById('main');
+  window.onscroll = function () { 
+      if ( document.documentElement.scrollTop >= 15 ) {
+          myNav.classList.add("nav-colored");
+          myNav2.classList.add("nav-colored");
+          // myNav.classList.remove("nav-transparent");
+      } 
+      else {
+          // myNav.classList.add("nav-transparent");
+          myNav.classList.remove("nav-colored");
+          myNav2.classList.remove("nav-colored");
+      }
+  };
+
+  GitHubCalendar(".calendar", "Mrkishansharma", {
+    responsive: true,
+    global_stats: true,
+    tooltips: true,
+  });
+
+  document.getElementById("resume-button-1").onclick=()=>{
+window.open("https://drive.google.com/drive/folders/1_f_84l56qhhHZD5nkrI5HA4fx8i48hXn")
+  }
+
+  document.getElementById("resume-button-2").onclick=()=>{
+   window.open("https://drive.google.com/drive/folders/1_f_84l56qhhHZD5nkrI5HA4fx8i48hXn")
+  }
+     
+  
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.display = "none";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.display= "block";
+  }
