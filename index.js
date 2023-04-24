73d2e92e93e25837aeea0cbf6f82c2c4b69673d6
Abhirup Bakshi <abhirup.bakshi@disroot.org>
2023-04-24 18:49:15 +0530

    portfolio

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
-let i=0;
-let txt="NodeJS Backend Developer.";
-let speed=150;
+let i = 0;
+let txt = "Java Backend Developer.";
+let speed = 150;
 
 typeWriter()
 
 function typeWriter() {
-    if(i < txt.length){
-      document.getElementById("user-detail-name").style.color="aqua";
-      document.getElementById("user-detail-name").style.fontWeight="Bold";
-      document.getElementById("demo1").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
-    }
-    else {
-      // reset the index when it reaches the end of the text
-      i = 0;
-      // clear the text
-      document.getElementById("demo1").innerHTML = "";
-      // make the recursive call
-      document.getElementById("user-detail-name").style.color="white";
-      setTimeout(typeWriter, speed);
-    }
+  if (i < txt.length) {
+    document.getElementById("user-detail-name").style.color = "aqua";
+    document.getElementById("user-detail-name").style.fontWeight = "Bold";
+    document.getElementById("demo1").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
   }
-
-
-  var myNav = document.getElementById('nav-menu');
-  var myNav2 = document.getElementById('main');
-  window.onscroll = function () { 
-      if ( document.documentElement.scrollTop >= 15 ) {
-          myNav.classList.add("nav-colored");
-          myNav2.classList.add("nav-colored");
-          // myNav.classList.remove("nav-transparent");
-      } 
-      else {
-          // myNav.classList.add("nav-transparent");
-          myNav.classList.remove("nav-colored");
-          myNav2.classList.remove("nav-colored");
-      }
-  };
-
-  GitHubCalendar(".calendar", "Mrkishansharma", {
-    responsive: true,
-    global_stats: true,
-    tooltips: true,
-  });
-
-  document.getElementById("resume-button-1").onclick=()=>{
-window.open("https://drive.google.com/file/d/1gv76COLhSycD0gHp7f1qYNEGyDGSH_Xj/view?usp=sharing")
+  else {
+    // reset the index when it reaches the end of the text
+    i = 0;
+    // clear the text
+    document.getElementById("demo1").innerHTML = "";
+    // make the recursive call
+    document.getElementById("user-detail-name").style.color = "white";
+    setTimeout(typeWriter, speed);
   }
+}
 
-  document.getElementById("resume-button-2").onclick=()=>{
-   window.open("https://drive.google.com/file/d/1gv76COLhSycD0gHp7f1qYNEGyDGSH_Xj/view?usp=sharing")
+
+var myNav = document.getElementById('nav-menu');
+var myNav2 = document.getElementById('main');
+window.onscroll = function () {
+  if (document.documentElement.scrollTop >= 15) {
+    myNav.classList.add("nav-colored");
+    myNav2.classList.add("nav-colored");
+    // myNav.classList.remove("nav-transparent");
   }
-     
-  
-  function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.display = "none";
+  else {
+    // myNav.classList.add("nav-transparent");
+    myNav.classList.remove("nav-colored");
+    myNav2.classList.remove("nav-colored");
   }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.display= "block";
-  }
+};
+
+GitHubCalendar(".calendar", "abhirupbakshi", {
+  responsive: true,
+  global_stats: true,
+  tooltips: true,
+});
+
+document.getElementById("resume-button-1").onclick = () => {
+  window.open("https://drive.google.com/file/d/1rqEOeSDcGGR5x_lKBTYMkL8cfnEShobQ/view?usp=share_link")
+}
+
+document.getElementById("resume-button-2").onclick = () => {
+  window.open("https://drive.google.com/file/d/1rqEOeSDcGGR5x_lKBTYMkL8cfnEShobQ/view?usp=share_link")
+}
+
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.display = "block";
+}
 
 
 
-document.getElementById('goTOHackerrank').addEventListener("click", ()=>{
+document.getElementById('goTOHackerrank').addEventListener("click", () => {
   location.href = 'https://www.hackerrank.com/kishansharma6377?hr_r=1'
 })
